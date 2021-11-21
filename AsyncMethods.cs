1a688cb406144823af215f9067f032cf376eb58b
F3T1W <particular0010abyss@gmail.com>
2021-11-22 02:33:20 +0300

    Architecture improved

---
+++ AsyncMethods.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakFAST
+{
+    class AsyncMethods
+    {
+        public static void MakeBreakfastAsync()
+        {
+            Console.WriteLine("Start to cook breakfast!");
+
+            var fryingEggs = FryEggsAsync(2);
+
+            var fryingBread = FryBreadSlicesAsync(2);
+
+            CutMushroom(5);
+
+            var friedMushroom = FryMushroomAsync();
+
+            Task.WaitAll(fryingEggs, fryingBread, friedMushroom);
+
+            Console.WriteLine("Breakfast ready! Have a nice meal");
+        }
+
+        public static async Task FryEggsAsync(int x)
+        {
+            Console.WriteLine("Frying eggs");
+
+            await Task.Delay(1000);
+
+            Console.WriteLine($"{x} fried eggs");
+        }
+
+        public static void CutMushroom(int x)
+        {
+            Console.WriteLine("Cutting mushrooms");
+
+            Task.Delay(500).GetAwaiter().GetResult();
+
+            Console.WriteLine($"{x} cutted mushrooms");
+
+        }
+
+        public static async Task FryMushroomAsync()
+        {
+            Console.WriteLine($"Frying cutted mushrooms");
+
+            await Task.Delay(3000);
+
+            Console.WriteLine("Mushrooms fried");
+        }
+
+        public static async Task FryBreadSlicesAsync(int x)
+        {
+            Console.WriteLine("Toasting bread");
+
+            await Task.Delay(2000);
+
+            Console.WriteLine($"{x} slices of bread fried");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -5,65 +5,12 @@ namespace BreakFAST
 {
     class Program
     {
-        
+
         static void Main()
         {
-             MakeBreakfastAsync();
+            AsyncMethods.MakeBreakfastAsync();
         }
 
-        public static void MakeBreakfastAsync()
-        {
-            Console.WriteLine("Start to cook breakfast!");
-
-            var fryingEggs = FryEggsAsync(2);
-
-            var fryingBread = FryBreadSlicesAsync(2);
-
-            CutMushroom(5);
-
-            var friedMushroom =  FryMushroomAsync();
-
-            Task.WaitAll(fryingEggs, fryingBread, friedMushroom);
-
-            Console.WriteLine("Breakfast ready! Have a nice meal");
-        }
-
-        public static async Task FryEggsAsync(int x)
-        {
-            Console.WriteLine("Frying eggs");
-
-            await Task.Delay(1000);
-
-            Console.WriteLine($"{x} fried eggs");
-        }
-
-        public static void CutMushroom(int x)
-        {
-            Console.WriteLine("Cutting mushrooms");
-
-            Task.Delay(500).GetAwaiter().GetResult();
-
-            Console.WriteLine($"{x} cutted mushrooms");
-
-        }
-
-        public static async Task FryMushroomAsync()
-        {
-            Console.WriteLine($"Frying cutted mushrooms");
-
-            await Task.Delay(3000);
-
-            Console.WriteLine("Mushrooms fried");
-        }
-
-        public static async Task FryBreadSlicesAsync(int x)
-        {
-            Console.WriteLine("Toasting bread");
-
-            await Task.Delay(2000);
-
-            Console.WriteLine($"{x} slices of bread fried");
-        }
     }
 }
 
